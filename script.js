7ae7a946cc22987f77941697bd95c5fe476e35f7
Carlosedu2009 <carloseduardodecampos2@gmail.com>
2024-06-15 09:09:53 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let client1 = 'naruto'
+let client2 = 'pablomarsal'
+let client3 = 'veiodahavam'
+console.log(client1)
+console.log(client2)
+console.log(client3)
+for(let i=2;i<31;i++){console.log( i +client1)}
